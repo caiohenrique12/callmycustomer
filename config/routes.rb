52466ea87dae9745720eac6be57600fa4b27261c
chenrique <caiohenriqueads@gmail.com>
2015-09-16 01:15:16 -0300

    Change welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   resources :call_services
   resources :services
   resources :components
-  resources :components
   resources :customers
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'call_services#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

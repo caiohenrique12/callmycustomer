4a1103c3390c0b0096118403e9169632cb7c69a7
chenrique <caiohenriqueads@gmail.com>
2015-09-18 13:56:46 -0300

    Configuring templates for global internationalize

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
 	
-	validates_presence_of :name, :adress, :phone1, :phone2, :age, :sex
+	validates_presence_of :name, :address, :phone1, :phone2, :age, :sex
 	
-	validates_length_of :name, :adress, :phone1, :phone2, :age, :sex, allow_blank: false
+	validates_length_of :name, :address, :phone1, :phone2, :age, :sex, allow_blank: false
 end

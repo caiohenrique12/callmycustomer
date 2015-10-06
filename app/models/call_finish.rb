a42e2e0f9eb679f5e1f4b4938201a384b150fc3a
chenrique <caiohenriqueads@gmail.com>
2015-10-06 18:02:31 -0300

    delete

--- app/models/call_finish.rb
+++
@@ -1,6 +0,0 @@
-class CallFinish < ActiveRecord::Base
-
-	validates_presence_of :id_call, :id_user, :observation, :active
-
-	validates_length_of :id_call, :id_user, :observation, :active, allow_blank: false
-end

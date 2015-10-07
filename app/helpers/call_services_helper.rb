95d549f4d73ed95bf85d138d8b1e64e45e1998a3
chenrique <caiohenriqueads@gmail.com>
2015-10-07 11:44:21 -0300

    change call service

--- app/helpers/call_services_helper.rb
+++ app/helpers/call_services_helper.rb
@@ -1,2 +1,7 @@
 module CallServicesHelper
+
+  def belongs_to_user(call_service)
+    use_signed_in? && call_service.user == current_user
+  end
+
 end
--- app/models/call_service.rb
+++ app/models/call_service.rb
@@ -1,7 +1,8 @@
 class CallService < ActiveRecord::Base
+    belongs_to :user
 
   	validates_presence_of :details
 
   	validates_length_of :details, allow_blank: false
-    
+
 end
